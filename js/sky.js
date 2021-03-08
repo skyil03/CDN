9087585e5a6c17af3d73e0b83068ce102e1dbab9
skyil03 <80210246+skyil03@users.noreply.github.com>
2021-03-08 22:46:19 +0800

    Add files via upload

---
+++ js/sky.js
@@ -0,0 +1,9 @@
+var full_page = document.getElementsByClassName("full_page");
+if (full_page.length != 0) {
+  full_page[0].style.background = "transparent";
+}
+
+var not_index = document.getElementsByClassName("not-index-bg");
+if (not_index.length != 0) {
+  not_index[0].style.background = "transparent";
+}
